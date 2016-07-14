f0c26ddf83b3ad92d06f60471ba873a90b35453d
Yusuke Oshiro <yusuke@oshiro1.com>
2016-07-14 17:27:59 +0900

    rake command fix

--- app/models/job.rb
+++ app/models/job.rb
@@ -30,7 +30,7 @@ class Job < ActiveRecord::Base
 		result = s.insert_record( "InsightsExternalData", payload )
 
 		if result[:success]
-			DataLoadWorker.perform_async(self.dataset_name, self.csv_url, self.username, self.password, self.meta_json,self.mobile, result[:record_id])			
+			perform(self.dataset_name, self.csv_url, self.username, self.password, self.meta_json,self.mobile, result[:record_id])			
 		end
 
 		self.next_job = get_next_job(self)
@@ -40,6 +40,10 @@ class Job < ActiveRecord::Base
 
 
 	private 
+
+
+
+
 	def update_next_job
 		if self.next_job.nil?
 			self.next_job = get_next_job(self)
@@ -56,5 +60,102 @@ class Job < ActiveRecord::Base
 			return DateTime.new(rightnow.year, rightnow.month, rightnow.day, job.hour_of_day, 0, 0)
 		end
 	end
+
+	def perform( dataset_name, csv_url, username, password, meta_json, phone, parent_record_id )
+		
+		require 'open-uri'
+		require 'pp'
+
+
+		payload = {
+			"Format" => "Csv",
+			"EdgemartAlias" => dataset_name,
+			"MetadataJson" => meta_json,
+			"Operation" => "Overwrite",
+			"Action" => "None"
+		}
+
+
+		original_file_name = "tmp_csv_file-" + SecureRandom.hex(5)
+		file_names = [ original_file_name+"-1" ] # contains the list smaller chunks of files
+		
+
+
+		#download the csv file to working directory
+		open(original_file_name, 'wb'){|saved_file|
+			open(csv_url, 'rb'){|read_file|
+				saved_file.write(read_file.read)
+			}
+		}
+		
+		#open source file and destination file
+		file = File.open(original_file_name, "r")
+		index_of_files = 0
+		tmp = File.open(file_names[index_of_files], "a")
+
+		file.each do |line|
+			# go through each line of the csv file
+			# and create create files all less than 5 MB
+			tmp.puts line
+
+			if tmp.size > FILE_MAX_SIZE
+				p "index is: #{index_of_files}"
+				tmp.close
+				index_of_files = index_of_files + 1
+				file_names << original_file_name + "-" + (index_of_files + 1).to_s
+				tmp = File.open(file_names[index_of_files], "a")
+			end
+		end
+		file.close
+		File.delete( original_file_name )
+
+
+		payload = {
+			"Format" => "Csv",
+			"EdgemartAlias" => dataset_name,
+			"MetadataJson" => meta_json,
+			"Operation" => "Overwrite",
+			"Action" => "None"
+		}
+
+		s = SforceWrapper.new(username, password)
+		
+		if parent_record_id
+			file_names.each_with_index do |file_name, index|
+				p "uploading part: #{file_name}"
+				payload = {
+					"DataFile" => Base64.encode64(File.read(file_name)),
+					"InsightsExternalDataId" => parent_record_id,
+					"PartNumber" => index+1
+				}
+				s.insert_record( "InsightsExternalDataPart", payload )
+				File.delete(file_name)
+			end
+
+
+			payload = {
+			"Action" => "Process"
+			}
+			s.update_record( "InsightsExternalData", parent_record_id, payload )
+			
+			if phone.present?
+				blowerio = RestClient::Resource.new(ENV['BLOWERIO_URL'])
+				blowerio['/messages'].post :to => phone, :message => "analytics load completed for dataset '#{dataset_name}'"
+			end
+			
+
+		else
+			p "data loading header could not be created!"
+			if phone.present?
+				blowerio = RestClient::Resource.new(ENV['BLOWERIO_URL'])
+				blowerio['/messages'].post :to => phone, :message => "analytics load failed for dataset '#{dataset_name}'"
+			end
+
+		end
+
+
+
+	end
+
 
 end
