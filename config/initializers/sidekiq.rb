005ae1195805296624080614e5957d075d4bbaec
Yusuke Oshiro <yoshiro@yoshiro-ltm.internal.salesforce.com>
2017-04-04 16:08:47 +0900

    changed max size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
+    config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq', size: 1 }
 end
 
 
